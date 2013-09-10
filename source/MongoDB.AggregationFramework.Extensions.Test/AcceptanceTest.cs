fba5d7541b1664a4d3a1b326a898a75a73b16d70
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2013-09-10 10:12:45 +0200

    first commit for solution infrastructure

---
+++ source/MongoDB.AggregationFramework.Extensions.Test/AcceptanceTest.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using NUnit.Framework;
+using System;
+
+namespace MongoDB.AggregationFramework.Extensions.Test
+{
+    [TestFixture]
+    public class AcceptanceTest
+    {
+        private MongoCollection _collection;
+
+        [Test]
+        public void DefiningFlows()
+        {
+            // Shows how pipeline could be created
+
+            //var pipeline = Pipeline.For<Document>()
+            //     .Match(d => d.Value).IsGreaterThan(10)
+            //         .And(d => d.EmbeddeDocument.Value).Is("test")
+            //     .Then.Project(d => d.Value)
+            //         .AndNot(d => d.Id);
+
+
+            // _collection.Aggregate(pipeline);
+        }
+
+        //public class Document
+        //{
+        //    public String Id { get; set; }
+        //    public Int32 Value { get; set; }
+        //    public Nested EmbeddedDocument { get; set; }
+
+        //    public class Nested
+        //    {
+        //        public String Value { get; set; }
+        //    }
+        //}
+    }
+}
