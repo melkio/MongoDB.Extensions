050adea0ef834129d09453ebfe9709e75b7cd066
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2013-10-15 21:54:42 +0200

    new version

--- source/VersionInfo.cs
+++ source/VersionInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCompany("CodicePlastico srl")]
 [assembly: AssemblyCopyright("Copyright © CodicePlastico 2013")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
